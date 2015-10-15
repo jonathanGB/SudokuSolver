4e4195cc5effad32d2ede09b8dec345fbc7ec3df
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2015-10-15 17:16:22 -0400

    starting to implementing the check

--- js/script.js
+++ js/script.js
@@ -16,16 +16,56 @@ function changeButtonState() {
 		$('#buttonSolve button').addClass('pure-button-disabled');
 }
 
+function fillMap() {
+	$('table tbody tr').each(function(i) {
+		$(this).find('td input').each(function(j) {
+			var value;
+
+			if ($(this).val() == '')
+				value = -1;
+			else
+				value = parseInt($(this).val())
+
+			board[i][j] = value;
+		});
+	});
+}
+
+function checkRow(value, index) {
+	;
+}
+
+function validateMap() {
+	$('table tbody td input').each(function(index) {
+		var valueOfCell = $(this).val();
+
+		if (valueOfCell != '') {
+			var col = index % 9;
+			var row = index / 9;
+
+			checkRow(valueOfCell, row);
+		}
+	});
+}
+
+function buildMap() {
+	fillMap();
+	//validateMap();
+}
+
 $(function() {
 	/* Global variables */
+
+	// create board Matrix
+	board = [];
+	for (var i = 0; i < 9; i++)
+		board[i] = new Array(9);
+
 
 	/* At start */
 	$('table tbody tr td').each(function(index) {
 		if ((index + 1) % 3 == 0)
 			$(this).addClass('big-border-right');
-
-		if ($(this).text() != '')
-			$(this).addClass('pre-chosen-numbers');
 	});
 
 	/* Events */
@@ -42,6 +82,12 @@ $(function() {
 	});
 
 	$('tbody input').on('keyup', changeButtonState);
+
+	$('#buttonSolve button').click(function() {
+		if (!$(this).hasClass('pure-button-disabled')) {
+			buildMap();
+		}
+	});
 
 	// find solution
 	// store it
