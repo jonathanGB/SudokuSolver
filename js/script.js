66ee4aa25cfec21b5cf6d77a3a5d7f666768b8a6
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2015-10-15 10:06:42 -0400

    input to solver, still need work

--- js/script.js
+++ js/script.js
@@ -7,6 +7,18 @@ $(function() {
 			$(this).addClass('pre-chosen-numbers');
 	});
 
+	$('tbody input').on('keydown paste', function(e) {
+		var BACKSPACE = 8, ZERO = 48, NINE = 57;
+		if (e.type === "paste")
+			e.preventDefault();
+		else {
+			var key = e.keyCode || e.charCode;
+			
+			if (this.value != '' && key != BACKSPACE && (key < ZERO || key > NINE))
+				e.preventDefault();
+		}
+	});
+
 	// find solution
 	// store it
 	// let the user play
