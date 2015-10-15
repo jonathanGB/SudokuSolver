363e12c048a3cefd13b5c4f5848d2f3d09d75fbc
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2015-10-15 10:35:34 -0400

    Added button - fixed input table
    function to be added, I wrote the declaration

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+/* Functions */
+function changeButtonState() {
+	; // to be added
+}
+
 $(function() {
+	/* Global variables */
+	var buttonState = 'pure-button-disabled';
+
+
+	/* At start */
 	$('table tbody tr td').each(function(index) {
 		if ((index + 1) % 3 == 0)
 			$(this).addClass('big-border-right');
@@ -7,6 +17,7 @@ $(function() {
 			$(this).addClass('pre-chosen-numbers');
 	});
 
+	/* Events */
 	$('tbody input').on('keydown paste', function(e) {
 		var BACKSPACE = 8, ZERO = 48, NINE = 57;
 		if (e.type === "paste")
@@ -14,8 +25,10 @@ $(function() {
 		else {
 			var key = e.keyCode || e.charCode;
 			
-			if (this.value != '' && key != BACKSPACE && (key < ZERO || key > NINE))
+			if (key != BACKSPACE && (this.value != '' || (key < ZERO || key > NINE)))
 				e.preventDefault();
+
+			changeButtonState();
 		}
 	});
 
