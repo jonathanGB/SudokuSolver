04fe019d0555d1b0674684018e0741d355bed3f5
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-01-02 01:01:00 -0500

    started commenting/restructuring sudokuSolver.js and added a favicon

--- js/sudokuSolver.js
+++ js/sudokuSolver.js
@@ -35,45 +35,51 @@ Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
 /* Functions */
 
+// checks if a row "i" in array "arr" is valid (a valid number must not be there more than once)
 function checkRow(i, arr) {
-	var row = new Array(SIZE_OF_BOARD);
+	var row = []; // holds the valid values in row "i"
 
 	for (var j = 0; j < SIZE_OF_BOARD; j++) {
 		var value = arr[i][j];
 
+		// if valid value and not already there in "row"
 		if (value && ~row.indexOf(value))
 			return false;
 		else
-			row.push(value);
+			row.push(value); // new valid value, add to "row"
 	}
 
 	return true;
 }
 
+// checks if all rows in array "arr" are valid
 function checkAllRows(arr) {
 	for (var i = 0; i < SIZE_OF_BOARD; i++) {
-		if (!checkRow(i, arr)) 
+		if (!checkRow(i, arr)) // if row "i" is invalid
 			return false;
 	}
 
 	return true;
 }
 
+// checks if column "j" in array "arr" is valid (a valid number must not be there more than once)
 function checkColumn(j, arr) {
-	var column = new Array(SIZE_OF_BOARD);
+	var column = []; // holds the valid values in column "j"
 
 	for (var i = 0; i < SIZE_OF_BOARD; i++) {
 		var value = arr[i][j];
 
+		// if valid value and not already there in "column"
 		if (value && ~column.indexOf(value))
 			return false;
 		else
-			column.push(value);
+			column.push(value); // new valid value, add to "column"
 	}
 
 	return true;
 }
 
+// checks if all columns in array "arr" are valid
 function checkAllColumns(arr) {
 	for (var j = 0; j < SIZE_OF_BOARD; j++) {
 		if (!checkColumn(j, arr))
@@ -83,24 +89,29 @@ function checkAllColumns(arr) {
 	return true;
 }
 
+// checks if square "i, j" in array "arr" is valid (a valid number must not be there more than once)
+// (i,j) is the coordinate of the top-left corner of the square
 function checkSquare(i, j, arr) {
-	var square = new Array(SIZE_OF_BOARD);
+	var square = []; // holds the valid values in square "i, j"
 
 	for (var k = i; k < i + SIZE_OF_SQUARE; k++) {
 		for (var l = j; l < j + SIZE_OF_SQUARE; l++) {
 			var value = arr[k][l];
 
+			// if valid value and not already there in "square"
 			if (value && ~square.indexOf(value))
 				return false;
 			else
-				square.push(value);
+				square.push(value); // new valid value, add to "square"
 		}
 	}
 
 	return true;
 }
 
+// checks if all squares in array "arr" are valid
 function checkAllSquares(arr) {
+	// the following two nested for loops traverse every top-left cell of every squares in the sudoku
 	for (var i = 0; i < SIZE_OF_BOARD; i += SIZE_OF_SQUARE) {
 		for (var j = 0; j < SIZE_OF_BOARD; j += SIZE_OF_SQUARE) {
 			if (!checkSquare(i, j, arr))
@@ -111,18 +122,20 @@ function checkAllSquares(arr) {
 	return true;
 }
 
-
-
+// validates a cell by row, column, and square ; all three must be true for a cell to be valid
 function validateCell(i, j, arr) {
 	var iSquare = rowOfSquare(i), jSquare = columnOfSquare(j);
 
 	return checkRow(i, arr) && checkColumn(j, arr) && checkSquare(iSquare, jSquare, arr);
 }
 
+// validates every cell in array "arr" by row, column and square ; all three must be true for cells to be valid
 function validateAllCells(arr) {
 	return checkAllRows(arr) && checkAllColumns(arr) && checkAllSquares(arr);
 }
 
+// initialize the Possibilities matrix with empty arrays as values (making it a 3-D array)
+// every position in a sudoku grid has an array of possible values, which Possibilities keeps track of
 function initializePossibilities() {
 	for (var i = 0; i < SIZE_OF_BOARD; i++) {
 		for (var j = 0; j < SIZE_OF_BOARD; j++) {
@@ -131,6 +144,7 @@ function initializePossibilities() {
 	}
 }
 
+// checks if array "arr" is filled with non-zero values, therefore if a solution is found or not
 function solutionFound(arr) {
 	for (var i = 0; i < SIZE_OF_BOARD; i++) {
 		for (var j = 0; j < SIZE_OF_BOARD; j++) {
@@ -142,33 +156,39 @@ function solutionFound(arr) {
 	return true;
 }
 
+// fill the Possibilities array
 function fillPossibilities(arr) {
 	var restart;
-	
+
 	do {
 		restart = false;
 
-		initializePossibilities();
+		initializePossibilities(); // initialize the Possibilities arrays
 
+		// loop through the whole grid
 		for (var i = 0; i < SIZE_OF_BOARD; i++) {
 			for (var j = 0; j < SIZE_OF_BOARD; j++) {
 				var value = arr[i][j];
 
+				// if value is falsey (0)
 				if (!value) {
+					// loop through all possible values (1 to 9)
 					for (var k = 1; k <= SIZE_OF_BOARD; k++) {
-						arr[i][j] = k;
+						arr[i][j] = k; // test value in array
 
+						// if k is valid, add to the possibilities of that cell
 						if (validateCell(i, j, arr))
 							possibilities[i][j].push(k);
 					}
 
+					// if the current empty cell has only one possibility, the only possible value is the right one
 					if (possibilities[i][j].length == 1) {
 						arr[i][j] = possibilities[i][j][0];
 						possibilities[i][j] = [];
-						restart = true;
+						restart = true; // restart the loop, as other empty cells can be affected by this change
 						break;
 					} else
-						arr[i][j] = 0;
+						arr[i][j] = 0; // we're not 100% sure, put the cell back to its unsolved state
 				}
 			}
 
@@ -180,6 +200,7 @@ function fillPossibilities(arr) {
 	console.log("finished first wave");
 }
 
+// to do
 function findHiddenPossibilities() {
 	var loop = 0, count = 0;
 	do {
@@ -198,7 +219,7 @@ function findHiddenPossibilities() {
 
 							if (possibilities[k][l].length == 1) {
 								board[k][l] = possibilities[k][l][0];
-								updatePossibilitiesTable(k, l, possibilities[k][l][0], true, true, true);
+								updatePossibilities(k, l, possibilities[k][l][0], true, true, true);
 								possibilities[k][l] = [];
 								count++;
 								loop = 0;
@@ -232,7 +253,7 @@ function findHiddenPossibilities() {
 							console.log("possible 1");
 							board[firstRow][firstColumn] = val;
 							possibilities[firstRow][firstColumn] = [];
-							updatePossibilitiesTable(firstRow, firstColumn, val, true, true, true);
+							updatePossibilities(firstRow, firstColumn, val, true, true, true);
 							count++;
 							loop = 0;
 						}
@@ -240,8 +261,8 @@ function findHiddenPossibilities() {
 						if (possibleCells > 1) {
 							console.log("possible > 1");
 							oneLine ? 
-								updatePossibilitiesTable(firstRow, firstColumn, val, true, false, false) :
-								updatePossibilitiesTable(firstRow, firstColumn, val, false, true, false);
+								updatePossibilities(firstRow, firstColumn, val, true, false, false) :
+								updatePossibilities(firstRow, firstColumn, val, false, true, false);
 						}
 					}
 				}
@@ -254,12 +275,14 @@ function findHiddenPossibilities() {
 	return count;
 }
 
-function updatePossibilitiesTable(row, column, val, updateRow, updateColumn, updateSquare) {
+// removes possibilities containing "val" in row "row", column "column" and square "row, column" depending on the updateX flags
+// squareException is optional... tells if we ignore a row or column in updateSquare, or a specific location [i, j] via an array
+function updatePossibilities(row, column, val, updateRow, updateColumn, updateSquare, squareException) {
 	var iSquare = rowOfSquare(row), jSquare = columnOfSquare(column);
 
 	if (updateRow) {
 		for (var j = 0; j < SIZE_OF_BOARD; j++) {
-			if (rowOfSquare(row) != iSquare || columnOfSquare(j) != jSquare) {
+			if (columnOfSquare(j) != jSquare) {
 				var index = possibilities[row][j].indexOf(val);
 
 				if (~index)
@@ -280,8 +303,19 @@ function updatePossibilitiesTable(row, column, val, updateRow, updateColumn, upd
 	}
 
 	if (updateSquare) {
+		var ignoreRow = -1, ignoreColumn = -1;
+
+		switch (squareException) {
+			case 'row': ignoreRow = row; break;
+			case 'column': ignoreColumn = column; break;
+		}
+
 		for (var i = iSquare; i < iSquare + SIZE_OF_SQUARE; i++) {
+			if (i == ignoreRow) continue;
+
 			for (var j = jSquare; j < jSquare + SIZE_OF_SQUARE; j++) {
+				if (j == ignoreColumn || Array.isArray(squareException) && squareException[0] == i && squareException[1] == j) continue;
+
 				var index = possibilities[i][j].indexOf(val);
 
 				if (~index)
@@ -291,10 +325,12 @@ function updatePossibilitiesTable(row, column, val, updateRow, updateColumn, upd
 	}
 }
 
+// returns the row of the square containing "i"
 function rowOfSquare(i) {
 	return parseInt(i / SIZE_OF_SQUARE) * SIZE_OF_SQUARE;
 }
 
+// returns the column of the square containing "j"
 function columnOfSquare(j) {
 	return parseInt(j / SIZE_OF_SQUARE) * SIZE_OF_SQUARE
 }
@@ -308,7 +344,7 @@ function onlyInRow() {
 				var index = -1, ctr = 0;
 
 				for (var j = 0; j < SIZE_OF_BOARD; j++) {
-					if (board[i][j] != val && ~possibilities[i][j].indexOf(val)) {
+					if (!board[i][j] && ~possibilities[i][j].indexOf(val)) {
 						if (index == -1)
 							index = j;	
 						else if (columnOfSquare(j) != columnOfSquare(index))
@@ -324,13 +360,14 @@ function onlyInRow() {
 						if (ctr == 1) {
 							board[i][index] = val;
 							possibilities[i][index] = [];
-							updatePossibilitiesTable(i, index, val, false, true, true);
+							updatePossibilities(i, index, val, false, true, true);
 							count++;
 							loop = 0;
 						} else if (ctr > 1) {
 							var j1 = columnOfSquare(index);
 
-							removeOtherOccasions(val, val, i, j1, i, j1 + 1, i, j1 + 2, val);
+							//removeOtherOccasions(val, val, i, j1, i, j1 + 1, i, j1 + 2, val);
+							updatePossibilities(i, index, val, false, false, true, 'row');
 						}
 					}
 				}
@@ -366,13 +403,14 @@ function onlyInColumn() {
 						if (ctr == 1) {
 							board[index][i] = val;
 							possibilities[index][i] = [];
-							updatePossibilitiesTable(index, i, val, true, false, true);
+							updatePossibilities(index, i, val, true, false, true);
 							count++;
 							loop = 0;
 						} else if (ctr > 1) {
 							var j1 = rowOfSquare(index);
 
-							removeOtherOccasions(val, val, j1, i, j1 + 1, i, j1 + 2, i, val);
+							//removeOtherOccasions(val, val, j1, i, j1 + 1, i, j1 + 2, i, val);
+							updatePossibilities(index, i, val, false, false, true, 'column');
 						}
 					}
 				}
@@ -395,7 +433,9 @@ function findTwoDuetsInSquare() {
 
 						for (var m = k; m < i + SIZE_OF_SQUARE; m++) {
 							for (var n = l; n < j + SIZE_OF_SQUARE; n++) {
-								if ((m != k || n != l) && arr.equals(possibilities[m][n])) {
+								if (m == k && n == l) continue;
+
+								if (arr.equals(possibilities[m][n])) {
 									var val1 = arr[0], val2 = arr[1];
 
 									console.log("duet found");
@@ -413,7 +453,7 @@ function findTwoDuetsInSquare() {
 function removeOtherOccasions(val1, val2, i1, j1, i2, j2, i3, j3, val3) {
 	var iSquare = rowOfSquare(i1), jSquare = columnOfSquare(j1);
 
-	if (i3 === undefined || j3 === undefined || val3 === undefined)
+	if (i3 === undefined)
 		i3 = j3 = val3 = -1;
 
 	for (var i = iSquare; i < iSquare + SIZE_OF_SQUARE; i++) {
