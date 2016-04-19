36ecadcc13e1196f08945fc5ab50cea91d5fa7ad
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-04-19 06:33:43 -0500

    1st version of computeFitness, may be enhance

--- src/geneticLib/geneticLib.go
+++ src/geneticLib/geneticLib.go
@@ -10,7 +10,7 @@ const BOARD_SIZE = 9
 const SQUARE_SIZE = 3
 
 type IndividualSolution struct {
-  fitness int8
+  fitness uint16
   solution map[int8]int8
 }
 
@@ -18,7 +18,7 @@ func (individual *IndividualSolution) addSolution(key int8, val int8) {
   individual.solution[key] = val
 }
 
-func (individual *IndividualSolution) setFitness(val int8) {
+func (individual *IndividualSolution) setFitness(val uint16) {
   individual.fitness = val
 }
 
@@ -38,13 +38,14 @@ func generatePopulation(size int, poss map[int8][]int8, grid [][]int8) []*Indivi
   for i := 0; i < size; i++ {
     ind := generateIndividual(poss, grid)
     pop = append(pop, ind)
+    fmt.Printf("%v\n\n", *ind)
   }
 
   return pop
 }
 
 func generateIndividual(poss map[int8][]int8, grid [][]int8) *IndividualSolution {
-  individual := IndividualSolution{-1, make(map[int8]int8, 0)}
+  individual := IndividualSolution{0, make(map[int8]int8, 0)}
 
   for key, val := range poss {
     randomInd := rand.Intn(len(val))
@@ -56,9 +57,11 @@ func generateIndividual(poss map[int8][]int8, grid [][]int8) *IndividualSolution
   return &individual
 }
 
-func computeFitness(solution map[int8]int8, grid [][]int8) int8 {
+func computeFitness(solution map[int8]int8, grid [][]int8) uint16 {
   var newBoard [9][9]int8
   var i, j int8
+  var count uint16 = 1
+
   // populate new board as a merge of the known board + random solution
   for i = 0; i < BOARD_SIZE; i++ {
     for j = 0; j < BOARD_SIZE; j++ {
@@ -70,6 +73,57 @@ func computeFitness(solution map[int8]int8, grid [][]int8) int8 {
     }
   }
 
-  fmt.Printf("%v\n\n", newBoard)
-  return -2
+  for i = 0; i < BOARD_SIZE; i++ {
+    for j = 0; j < BOARD_SIZE; j++ {
+      count += inRow(&newBoard, i, j + 1, newBoard[i][j])
+      count += inCol(&newBoard, i + 1, j, newBoard[i][j])
+      count += inSqu(&newBoard, i, j, newBoard[i][j])
+    }
+  }
+
+  return count
+}
+
+func inRow(board *[9][9]int8, i, j, val int8) uint16 {
+  for ; j < BOARD_SIZE - 1; j++ {
+      if board[i][j] == val {
+        return 1
+      }
+  }
+
+  return 0
+}
+
+func inCol(board *[9][9]int8, i, j, val int8) uint16 {
+  for ; i < BOARD_SIZE - 1; i++ {
+    if board[i][j] == val {
+      return 1
+    }
+  }
+
+  return 0
+}
+
+func inSqu(board *[9][9]int8, i, j, val int8) uint16 {
+  var rowSquare int8 = i / SQUARE_SIZE * SQUARE_SIZE
+  var colSquare int8 = j / SQUARE_SIZE * SQUARE_SIZE
+  var iSqu, jSqu int8
+
+  for iSqu = rowSquare; iSqu < rowSquare + SQUARE_SIZE; iSqu++ {
+    if iSqu < i {
+      continue
+    }
+
+    for jSqu = colSquare; jSqu < colSquare + SQUARE_SIZE; jSqu++ {
+      if iSqu == i && jSqu < j {
+        continue
+      }
+
+      if board[iSqu][jSqu] == val {
+        return 1
+      }
+    }
+  }
+
+  return 0
 }
